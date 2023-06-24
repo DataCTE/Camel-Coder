25680a249a83198b9f89bed0716a435578b0f02e
DataCTE <105170707+DataCTE@users.noreply.github.com>
2023-06-24 11:17:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-
-from file_operations import write_conversation_to_file
-from file_operations import generate_file_structure_and_scripts
-from tot import ProblemSolver
-from config import serpapi_api_key, conversation_directory 
-from agents import user_role_name, assistant_role_name, thoughtful_role_name, coding_role_name
-from agents import specified_task_msg, assistant_inception_prompt, thoughtful_inception_prompt, coding_inception_prompt
+from file_operations import write_conversation_to_file, generate_file_structure_and_scripts
+from file_interactor import FileInteractor
+from agents import (
+    specified_task_msg, assistant_inception_prompt, thoughtful_inception_prompt, coding_inception_prompt,
+    user_role_name, assistant_role_name, thoughtful_role_name, coding_role_name,
+    user_agent, user_inception_prompt, MonitorAgent, assistant_agent, coding_agent, thoughtful_agent,
+    specified_task
+)
 from langchain.prompts.chat import SystemMessage, HumanMessage, AIMessage
+from config import TOKEN_LIMIT, task, serpapi_api_key, conversation_directory 
+from tot import ProblemSolver
 import openai
-from agents import user_agent, user_inception_prompt, MonitorAgent, assistant_agent, coding_agent, thoughtful_agent, specified_task
-from langchain.callbacks import get_openai_callback
-from config import TOKEN_LIMIT, task
 
+# Initialize your FileInteractor
+file_interactor = FileInteractor(folder_path)
+
+# Initialize conversation
 conversation = []
 total_tokens = 0
 preserve_last_complete_message = True
@@ -23,8 +27,13 @@ user_agent.init_messages()
 user_agent.update_messages(user_inception_msg)
 
 # Initialize the MonitorAgent
-monitor_agent_class = MonitorAgent(task, "gpt-3.5-turbo-16k")
-
+monitor_agent_class = MonitorAgent(
+    """The goal of the agents in this program is to continually improve the existing codebase 
+    while preserving existing functionality. This means that any updates or modifications made 
+    to the agents' operations should aim to increase efficiency, readability, or capabilities 
+    without negatively affecting the outcomes they currently produce.""",
+    "gpt-3.5-turbo-16k"
+)
 
 # Add agents to the list
 agents = [
@@ -35,59 +44,106 @@ agents = [
 ]
 
 # Set the number of loops for user, assistant, and thoughtful agents
-loop_count = 3
+loop_count = 2
 
 # Set the number of main loops before running the coding agent and monitor agent intervention
-main_loops_before_coding = 3
-main_loops_before_monitor_intervention = 6
+main_loops_before_coding = 2
+main_loops_before_monitor_intervention = 5
 
 problem_solver = ProblemSolver(
-    openai_key=openai.api_key, 
+    openai_key=openai.api_key,
     serpapi_api_key=serpapi_api_key
 )
 
+# Initialize codebase content list
+codebase_content = []
 
-# Main conversation loop
-with get_openai_callback() as cb:
-    chat_turn_limit = 50
-    main_loop_count = 0
+# Get the files from the folder
+files = file_interactor.get_files()
+
+separator_line = "\n" + "=" * 60 + "\n"
+
+# Iterate over each file
+for file_index, file_path in enumerate(files):
+    print(f"Processing file: {file_path}")
+
+    # Get the lines from the file
+    lines = file_interactor.get_lines(file_path)
+
+    # Join the lines to get the file content
+    file_content = "\n".join(lines)
+
+    # Append the file content to the codebase content list
+    codebase_content.append(file_content)
+
+    # After you've received the response from the coding agent
+    updated_file_content = file_content
+
+    # Write the updated file content to the file
+    file_interactor.save_file(file_path, updated_file_content)
+
+    # Initialize conversation for the current file
+    conversation = []
+    user_agent.init_messages()
+    user_agent.update_messages(user_inception_msg)
 
     # Set the goal for the ProblemSolver at the start of the conversation loop
-    problem_solver.set_goal(specified_task_msg, conversation)
+    problem_solver.set_goal(
+        """The goal of the agents in this program is to continually improve the existing codebase 
+        while preserving existing functionality. This means that any updates or modifications made 
+        to the agents' operations should aim to increase efficiency, readability, or capabilities 
+        without negatively affecting the outcomes they currently produce.""",
+        conversation
+    )
+    
+    # Main conversation loop
+    chat_turn_limit = 50
+    main_loop_count = 0
 
     for n in range(chat_turn_limit):
         separator_line = "\n" + "=" * 60 + "\n"
 
-        # User, Assistant, Thoughtful loop
+         # User, Assistant, Thoughtful loop
         for _ in range(loop_count):
             for i, (role_name, agent, MessageClass, inception_msg) in enumerate(agents[:-1]):
-                if n == 1 and role_name == user_role_name:
-                    ai_msg = agent.step(inception_msg)
-                else:
-                    # Gather previous agent messages excluding the current agent's own responses
-                    prev_agent_responses = [msg[1] for msg in conversation if msg[0] != role_name]
+                # Read the file content
+                file_content = file_interactor.read_file(file_path)
 
-                    # Filter out messages that are not strings or AIMessage objects
-                    prev_agent_responses = [msg for msg in prev_agent_responses if isinstance(msg, str) or isinstance(msg, AIMessage)]
+                # Gather previous agent messages excluding the current agent's own responses
+                prev_agent_responses = [msg[1] for msg in conversation if msg[0] != role_name]
 
-                    # Extract the content from each message and join them with a newline
-                    message_content = "\n".join([msg.content if isinstance(msg, AIMessage) else msg for msg in prev_agent_responses[-2:]])
-                    ai_msg = agent.step(AIMessage(content=message_content))
+                # Combine the file content with the previous agent responses (excluding the current agent's own response)
+                all_messages = [file_content] + prev_agent_responses
 
-                msg = MessageClass(content=ai_msg.content)
-                conversation.append((role_name, msg.content))
-                total_tokens += len(msg.content.split())
+                # Filter out messages that are not strings or AIMessage objects
+                all_messages = [msg for msg in all_messages if isinstance(msg, str) or isinstance(msg, AIMessage)]
+
+                # Extract the content from each message and join them with a newline
+                message_content = "\n".join(
+                    [msg.content if isinstance(msg, AIMessage) else msg for msg in all_messages])
+
+                # Feed the previous agent responses and file content into the agent's step function
+                ai_msg = agent.step(AIMessage(content=message_content))
+
+                # Print agent message to screen
                 print(separator_line)
-                print(f"\n{'-' * 50}\n{role_name}:\n{'-' * 50}\n{msg.content}\n")
+                print(f"\n{'-' * 50}\n{role_name} Message:\n{'-' * 50}\n{ai_msg.content}\n")
                 print(separator_line)
+                # Add agent message to conversation history
+                conversation.append((role_name, ai_msg))
 
-                if total_tokens > TOKEN_LIMIT:
-                    print("Token limit exceeded. Truncating conversation.")
-                    if preserve_last_complete_message:
-                        last_complete_message = "\n".join([msg.content for _, _, msg, _ in agents[i-1:i-2]])
+                # Update the file content after the coding agent's loop
+                if main_loop_count % main_loops_before_coding == 0:
+                    # Read the file content
+                    file_content = file_interactor.read_file(file_path)
 
-            # Increment the main_loop_count after one full loop
-            main_loop_count += 1
+                    # Generate the updated file content based on the codebase content
+                    updated_file_content = "\n".join(codebase_content)
+
+                    # Verify if the updated file content matches the expected content
+                    if not file_interactor.verify_file_content(file_path, updated_file_content):
+                        print("File content verification failed. Aborting.")
+                        break
 
         # ProblemSolver methods
         brainstorming_response = problem_solver.brainstorm()
@@ -126,13 +182,6 @@ with get_openai_callback() as cb:
         print(f"\n{'-' * 50}\nProblemSolver - Decision Response:\n{'-' * 50}\n{decision_response}\n")
         print(separator_line)
 
-        final_product_response = problem_solver.produce_final_product(specified_task)
-        conversation.append(("ProblemSolver", "Final Product Response: " + final_product_response))
-        print(separator_line)
-        print(f"\n{'-' * 50}\nProblemSolver - Final Product Response:\n{'-' * 50}\n{final_product_response}\n")
-        print(separator_line)
-
-
         # Coding agent loop after main_loops_before_coding full main loops
         if main_loop_count % main_loops_before_coding == 0:
             role_name, coding_agent, MessageClass, coding_inception_msg = agents[-1]
@@ -153,47 +202,38 @@ with get_openai_callback() as cb:
 
             # Gather most recent responses from user, assistant, thoughtful agents, and problem solver
             most_recent_responses = "\n".join([msg[1] for msg in conversation[-4:] if msg[0] != role_name])
-            
-            # Generate the file structure and scripts based on the file structure content
-            file_structure_prompt = (
-                f"As the {coding_role_name}, based on the previous main loop, refinement, and most recent responses, please generate a hypothetical file structure "
-                f"that would be suitable for this coding project. Refrain from any test files this is to be working prototype. There must be absolutely no test files or folders!\n\n"
+
+            # Generate the updated file content based on the previous main loop, refinement, and most recent responses
+            updated_file_content_prompt = (
+                f"As the {coding_role_name}, based on the previous main loop, refinement, and most recent responses, please rewrite the file content with the improvement suggestions.\n\n"
                 f"Most recent responses:\n{most_recent_responses}\n\n"
                 f"{prev_main_loop}"
             )
-            file_structure_ai_msg = coding_agent.step(MessageClass(content=file_structure_prompt))
-            file_structure_msg = MessageClass(content=file_structure_ai_msg.content)
-            conversation.append((role_name, file_structure_msg))
-            total_tokens += len(file_structure_msg.content.split())
+            updated_file_content_ai_msg = coding_agent.step(MessageClass(content=updated_file_content_prompt))
+            updated_file_content_msg = MessageClass(content=updated_file_content_ai_msg.content)
+            conversation.append((role_name, updated_file_content_msg))
+            total_tokens += len(updated_file_content_msg.content.split())
             print(separator_line)
-            print(f"\n{'-' * 50}\n{role_name}:\n{'-' * 50}\n{file_structure_msg.content}\n")
+            print(f"\n{'-' * 50}\n{role_name}:\n{'-' * 50}\n{updated_file_content_msg.content}\n")
             print(separator_line)
 
-            # After you've received the response from the Python Coding Expert
-            response = file_structure_msg.content  # Replace with actual response content
+            # After you've received the response from the coding agent
+            updated_file_content = updated_file_content_msg.content
 
-            # Extract the file structure content from the response
-            file_structure_content = response.split('```')[1].strip() 
-
-            # Generate file structure
-            generate_file_structure_and_scripts(file_structure_content, coding_agent, conversation_directory)
-
-            # Print message
+            # Print the updated file content to the screen
             print(separator_line)
-            print(f"\n{'-' * 50}\n{role_name}:\n{'-' * 50}\n{file_structure_msg.content}\n")
+            print(f"\n{'-' * 50}\nUpdated File Content:\n{'-' * 50}\n{updated_file_content}\n")
             print(separator_line)
-            
+
+            # Write the updated file content to the file
+            file_interactor.write_content(file_path, updated_file_content)
+
         # MonitorAgent intervention
-        conversation_str = " ".join([msg[1] for msg in conversation])
+        conversation_str = " ".join([str(msg[1]) for msg in conversation])
         if monitor_agent_class.should_intervene(conversation_str):
             intervention_message = monitor_agent_class.stage_intervention(conversation_str)
             print(f"\n{'-' * 50}\nMonitorAgent Intervention:\n{'-' * 50}\n{intervention_message}\n")
             conversation.append(("MonitorAgent", intervention_message))
-                
-    print(f"Total Successful Requests: {cb.successful_requests}")
-    print(f"Total Tokens Used: {cb.total_tokens}")
-    print(f"Prompt Tokens: {cb.prompt_tokens}")
-    print(f"Completion Tokens: {cb.completion_tokens}")
-    print(f"Total Cost (USD): ${cb.total_cost}")
+
 
 write_conversation_to_file(conversation, 'conversation.txt')
