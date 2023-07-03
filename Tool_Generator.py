b98416493dd57c87b2b969ed7fb6e752ff4ecb25
DataCTE <105170707+DataCTE@users.noreply.github.com>
2023-07-03 20:31:17 +0000

    Add files via upload

---
+++ Tool_Generator.py
@@ -0,0 +1,15 @@
+class ToolGenerator:
+    def __init__(self, model):
+        self.model = model
+
+    def tool_proposing(self, training_demonstrations):
+        # TODO: Implement tool proposing logic
+        pass
+
+    def tool_verification(self, validation_samples):
+        # TODO: Implement tool verification logic
+        pass
+
+    def tool_wrapping(self):
+        # TODO: Implement tool wrapping logic
+        pass
